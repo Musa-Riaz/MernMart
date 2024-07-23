9f64d4f8e2d456d4d60d81916ec06c20073ca9a0
Musariaz <riazmusa69@gmail.com>
2024-07-23 16:50:19 +0500

    made a build
    made a build folder

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const authRoutes = require('./routes/authRoute');
 const categoryRoutes = require('./routes/categoryRoutes');
 const productRoutes = require('./routes/productRoutes');
 const cors = require('cors');
+const file = require('file');
 //configure env
 dotenv.config({path : './config.env'});
 
@@ -19,7 +20,7 @@ const app = express();
 app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, "./client/build")))
 
 //routes
 app.use('/api/v1/auth', authRoutes);
@@ -27,11 +28,10 @@ app.use('/api/v1/category', categoryRoutes);
 app.use('/api/v1/product', productRoutes);
 
 //rest api
-app.get('/', (req, res) =>{
-    res.send({
-        message:"Welcome to the Node.js API"
-    })
+app.use("*", function(req, res){
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
 })
+
 
 const PORT = process.env.PORT || 8080;
 
